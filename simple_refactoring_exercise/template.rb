3f90980dfa9eac242094de84975a9d1f0cbe9a6c
sitniks <alexander_sitnikov@yahoo.com>
2022-02-08 11:12:19 -0500

    Handle %CODE at the beginning of the string; Extract two private substitute functions

--- simple_refactoring_exercise/template.rb
+++ simple_refactoring_exercise/template.rb
@@ -1,19 +1,33 @@
 module Template
   def template(source_template, req_id)
     template = String.new(source_template)
+    code = String.new(req_id)
 
-    # Substitute for %CODE%
+    template = substitute_for_code(template, code)
+    template = substitute_for_altcode(template, code)
+    template
+  end
+
+  private 
+
+  def substitute_for_code(template, code)
     template_split_begin = template.index("%CODE%")
     template_split_end = template_split_begin + 6
-    template_part_one =
-      String.new(template[0..(template_split_begin-1)])
+
+    template_part_one = ''
     template_part_two =
       String.new(template[template_split_end..template.length])
-    code = String.new(req_id)
+    
+    if template_split_begin > 0
+      template_part_one =
+        String.new(template[0..(template_split_begin-1)])
+    end
+    
     template =
       String.new(template_part_one + code + template_part_two)
+  end
 
-    # Substitute for %ALTCODE%
+  def substitute_for_altcode(template, code)
     template_split_begin = template.index("%ALTCODE%")
     template_split_end = template_split_begin + 9
     template_part_one =
--- simple_refactoring_exercise/template_spec.rb
+++ simple_refactoring_exercise/template_spec.rb
@@ -7,4 +7,8 @@ describe Template do
     expect(template('Code is %CODE%; alt code is %ALTCODE%', '5678901234')).to eq 'Code is 5678901234; alt code is 56789-012'
   end
 
+  it "should substitute '%CODE%%ALTCODE%'" do
+    expect(template('%CODE%%ALTCODE%', '5678901234')).to eq '567890123456789-012'
+  end
+
 end
