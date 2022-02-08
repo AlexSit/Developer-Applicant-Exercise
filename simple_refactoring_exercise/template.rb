44d7495c86b4d092f543b7e80e9ba4178d777dc8
sitniks <alexander_sitnikov@yahoo.com>
2022-02-08 14:56:00 -0500

    Move templating into a class

--- simple_refactoring_exercise/template.rb
+++ simple_refactoring_exercise/template.rb
@@ -1,18 +1,63 @@
 module Template
   MINIMUM_CODE_LENGTH = 8
 
+  class TemplateFiller
+    def initialize(template)
+      @template=template
+    end
+
+    def fill(match_pattern, replacement)
+      @template = substitute_first_occurence(match_pattern, replacement)
+    end
+
+    def get_template()
+      @template
+    end
+
+    private
+
+    # NOTE: I could have used built-in regex/replace functions and although this can be done easily
+    # I'd rather demonstrate my refactoring using this "manual" template transformation
+    def substitute_first_occurence(match_pattern, replacement)
+      template_split_begin = @template.index(match_pattern)
+      return @template if template_split_begin.nil?
+
+      template_split_end = template_split_begin + match_pattern.length
+
+      template_part_one = make_template_part_one(template_split_begin)
+      template_part_two = make_template_part_two(template_split_end)
+
+      template_part_one + replacement + template_part_two
+    end
+
+    def make_template_part_one(template_split_begin)
+      template_part_one_is_empty = template_split_begin == 0
+      return '' if template_part_one_is_empty
+
+      template_part_one_end_index = template_split_begin - 1
+      substring(@template, 0, template_part_one_end_index)
+    end
+
+    def make_template_part_two(template_split_end)
+      substring(@template, template_split_end, @template.length)
+    end
+
+    def substring(str, start_index, end_index)
+      String.new(str[start_index..end_index])
+    end
+  end
+
   def template(template, code)
     validate_source_template(template)
     validate_code(code)
 
     altcode = transform_code_into_altcode(code)
 
-    template = substitute_first_occurence(template, "%CODE%", code)
-    template = substitute_first_occurence(template, "%ALTCODE%", altcode)
-    template
+    template_filler = TemplateFiller.new(template)
+    template_filler.fill("%CODE%", code)
+    template_filler.fill("%ALTCODE%", altcode)
+    template_filler.get_template()
   end
-
-  private
 
   def validate_source_template(template)
     raise ArgumentError.new(
@@ -40,35 +85,5 @@ module Template
   def transform_code_into_altcode(code)
     code[0..4] + "-" + code[5..7]
   end
-
-  # NOTE: I could have used built-in regex/replace functions and although this can be done easily
-  # I'd rather demonstrate my refactoring using this "manual" template transformation
-  def substitute_first_occurence(template, match_pattern, replacement)
-    template_split_begin = template.index(match_pattern)
-    return template if template_split_begin.nil?
-
-    template_split_end = template_split_begin + match_pattern.length
-
-    template_part_one = make_template_part_one(template, template_split_begin)
-    template_part_two = make_template_part_two(template, template_split_end)
-
-    template_part_one + replacement + template_part_two
-  end
-
-  def make_template_part_one(template, template_split_begin)
-    template_part_one_is_empty = template_split_begin == 0
-    return '' if template_part_one_is_empty
-
-    template_part_one_end_index = template_split_begin - 1
-    substring(template, 0, template_part_one_end_index)
-  end
-
-  def make_template_part_two(template, template_split_end)
-    substring(template, template_split_end, template.length)
-  end
-
-  def substring(str, start_index, end_index)
-    String.new(str[start_index..end_index])
-  end
-
 end
+
